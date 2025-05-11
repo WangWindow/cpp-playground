2278055a46652501b894cd35e8b450a0e5eef241
WangWindow <1598593280@qq.com>
2025-05-11 18:43:19 +0800

    [update]: update `main.cpp`, add a header comment.

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,14 @@
+/*
+==============================================================================
+ * @FilePath: main.cpp
+ * @Author: WangWindow 1598593280@qq.com
+ * @Date: 2025-05-10 20:36:11
+ * @LastEditors: WangWindow
+ * @LastEditTime: 2025-05-11 18:41:43
+ * @2025 by WangWindow, All Rights Reserved.
+ * @Descripttion:
+==============================================================================
+*/
 import hello;
 
 auto main() -> int {
