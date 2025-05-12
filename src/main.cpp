8dd1724b80a9065900460da5744374c9d70304a5
WangWindow <1598593280@qq.com>
2025-05-12 16:47:05 +0800

    [update]: `move`, 所有权转移

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,40 @@
 import std;
 using namespace std;
 
+class X {
+public:
+    X() {
+        println("默认构造函数");
+    }
+
+    X(const X&) {
+        println("拷贝构造函数");
+    }
+
+    auto operator=(X&& x) noexcept {
+        println("移动赋值运算符");
+    }
+};
+
+void f(X&& x);
+void f1(X&& x);
+void f2(X&& x);
+
+void f(X&& x) {
+    cout << &x << endl;
+    f1(move(x));
+}
+
+void f1(X&& x) {
+    cout << &x << endl;
+    f2(move(x));
+}
+
+void f2(X&& x) {
+    cout << &x << endl;
+}
+
 auto main() -> int {
-    println("Hello, World!");
+    X x;
+    f(move(x));
 }
