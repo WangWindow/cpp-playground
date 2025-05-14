bb023bb1b06be5fd378f9c6ac7dd23f035f8242f
WangWindow <1598593280@qq.com>
2025-05-14 19:26:26 +0800

    [update]: template <template <class T1, class T2, class T3> class S>
    模板模板形参

--- src/main.cpp
+++ src/main.cpp
@@ -11,9 +11,16 @@
 */
 import std;
 
-auto main() -> int {
-    class std {};
+template <template <class T1, class T2, class T3> class S>
+class Str {
+public:
+    S<char, std::char_traits<char>, std::allocator<char>> str;
+};
 
-    // std::print("Hello, World!"); // 错误，类 "std" 没有成员 "print"
-    ::std::print("Hello, World!"); // 正确，使用全局命名空间的 std::print
+auto main() -> int {
+    // std::basic_string 是通用模板
+    // std::string 是它的特化
+    Str<std::basic_string> s;
+    s.str = "hello world";
+    std::println("{}", s.str);
 }
