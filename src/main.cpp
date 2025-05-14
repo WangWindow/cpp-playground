962e9ac386e632b9d12b8e645f15120815d82411
WangWindow <1598593280@qq.com>
2025-05-14 20:35:07 +0800

    [update]: 形参包作为父类

--- src/main.cpp
+++ src/main.cpp
@@ -11,49 +11,19 @@
 */
 import std;
 
-template <size_t... args>
-class A {
+template <class... Ts>
+class Overload : public Ts... {
 public:
-    void f() {
-        // 形参包展开
-        for (const auto& i : { args... }) {
-            std::print("{}", i);
-        }
-    }
+    using Ts::operator()...;
 };
-
-template <typename... Args>
-void f(Args... args) {
-    (
-        // 形参包展开
-        std::print("{}", args),
-        ...); // C++17 折叠表达式
-}
-
-template <std::integral... Args>
-void f2(Args... args) {
-    (std::print("{}", args), ...);
-}
-
-template <template <class T1, class T2> class... args>
-class B {};
-
-// 示例
-template <size_t... args>
-void ff() {
-    size_t array[sizeof...(args)]{ args... };
-    for (const auto& i : array) {
-        std::print("{}", i);
-    }
-}
+// 形参列表可以被继承
 
 auto main() -> int {
-    A<1, 2, 3, 4, 5> a;
-    a.f();
+    auto a = Overload{ [](int arg) { std::println("int: {}", arg); },
+                       [](double arg) { std::println("double: {}", arg); },
+                       [](auto arg) { std::println("auto: {}", arg); } };
 
-    f(1, 2, 3, 4, "*");
-    endl(std::cout);
-    // f2(1, 2, 3, 4, "*"); // 需要 std::integral 概念约束
-
-    ff<1, 2, 3, 4, 5>();
+    a(1);
+    a(1.0);
+    a("hello");
 }
