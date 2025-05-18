1fc9c10d870d2cc1d7e9f65755d306c8fb6a99c4
WangWindow <1598593280@qq.com>
2025-05-18 19:12:18 +0800

    [update]: 约束表达式(涉及concept、 requires等)

--- src/main.cpp
+++ src/main.cpp
@@ -4,21 +4,47 @@
  * @Author: WangWindow 1598593280@qq.com
  * @Date: 2025-05-10 20:36:11
  * @LastEditors: WangWindow
- * @LastEditTime: 2025-05-11 18:41:43
+ * @LastEditTime: 2025-05-18 18:46:36
  * @2025 by WangWindow, All Rights Reserved.
  * @Descripttion:
 ==============================================================================
 */
+
 import std;
 
-template <class T, class... Args>
-void print(T v, Args... args) {
-    std::println("{}", v);
-    if constexpr ((0 <=> sizeof...(args)) < 0) {
-        print(args...);
-    }
+// 'concept' 是 C++20 中引入的一个新特性，用于定义类型约束
+// 下面的代码定义了一个名为 'is_int' 的概念，它检查一个类型是否是 'int'
+template <class T>
+concept is_int = std::is_same_v<T, int>;
+
+void test(is_int auto a) {
+    std::println("is int");
+}
+
+// 这段代码与上面的等价
+template <is_int T>
+void test_1(T a) {
+    std::println("is int");
+}
+
+// 约束表达式支持使用 '&&' 和 '||' 进行组合，以实现更复杂的约束
+
+// ‘require’ 只需要能通过编译就可以了
+template <class T>
+concept Test = requires(T a, T b) {
+    { a + b } -> is_int; // 复合约束
+};
+
+template <class T>
+void test_2(T a)
+    requires Test<T>
+{
+    std::println("is int");
 }
 
 auto main() -> int {
-    print("Hello, World!", 1, 2.0, "3", '4');
-} 
+    test(1);   // 传入一个 int 类型的参数
+    test_1(1); // 传入一个 int 类型的参数
+    test_2(1); // 传入一个 int 类型的参数
+    // test(1.0); // 传入一个 double 类型的参数，这行代码会导致编译错误，因为它不满足 is_int 的约束
+}
