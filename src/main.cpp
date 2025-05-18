6044d4f3f39c2da4acdbd616458fc9d72c6c1f49
WangWindow <1598593280@qq.com>
2025-05-18 15:15:34 +0800

    [update]: 函数形参作用域（注意：声明符是否为定义的声明符和 try 块）

--- src/main.cpp
+++ src/main.cpp
@@ -12,64 +12,22 @@
 
 #include <print>
 
-// 待决名：
-// 在模板定义中，某些构造的含义可以在不同的实例化之间有所不同，
-// 特别是，类型和表达式可能会取决于类型模板形参类型和非类型模板形参的值。
+// 函数形参作用域：函数形参（包括 lambda 表达式和形参）或函数局部预定义变量潜在作用域开始于其声明点。
+// - 若最内层的外围函数声明符不是定义的声明符，则潜在作用域结束于该函数声明符的结尾。
+// - 否则，其潜在作用域结束于函数 try 块的最后异常处理块的结尾（若不使用 try 块，则为函数体的结尾）。
 
-// 绑定规则：非待决名在模板定义点查找并绑定，
-// 即使在模板实例化点有更好的匹配，也保持此绑定。
-
-template <class T>
-class X {
-public:
-    void f() {
-        std::println("X::f()");
-    }
-};
-
-template <class T>
-class Y : public X<T> {
-public:
-    void ff() {
-        // f(); // 'f' 是非待决名，现在绑定
-        // error: there are no arguments to ‘f’ that depend on a template parameter,
-        // so a declaration of ‘f’ must be available [-Wtemplate-body]
-        // 解决方法：使用this->f()来调用基类的成员函数
-        this->f(); // 需要使用this来指明
-    }
-};
-
-template <class T>
-class U {
-public:
-    using V = const typename T::value; // typename 关键字用于告诉编译器 T::value 是一个类型，而不是静态成员
-};
-
-class O {
-public:
-    using value = int;
-};
-
-template <class T>
-class S {
-public:
-    template <class T1>
-    void foo() {
-    }
-};
-
-template <class T>
-void bar() {
-    S<T> s;
-    // s.foo<int>(); // error: '<' 会被解析为小于运算符
-    s.template foo<int>(); // 需要使用template来指明
+void f(int a) try {
+    int v{}; // v 不是定义的声明符，所以作用域结束于函数体的结尾，即 throw 语句的结尾
+    throw "";
+} catch (...) {
+    std::println("{}", a); // a 是函数形参，是定义的声明符，所以作用域结束于 try 块的最后异常处理块的结尾
 }
 
 auto main() -> int {
-    Y<int> y;
-    y.ff(); // 调用Y<int>的ff()函数
+    f(1);
 
-    U<O>::V u = 12; // -> O::value u
-
-    bar<int>();
+    int a{};
+    {
+        int a{}; // 两者的作用域显然不同，不会冲突
+    }
 }
