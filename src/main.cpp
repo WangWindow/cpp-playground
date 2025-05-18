ecd7ca4bd989b2858c02b7608231420f0b6352a5
WangWindow <1598593280@qq.com>
2025-05-18 21:03:36 +0800

    [update]: 管道运算

--- src/main.cpp
+++ src/main.cpp
@@ -12,39 +12,24 @@
 
 import std;
 
-// 'concept' 是 C++20 中引入的一个新特性，用于定义类型约束
-// 下面的代码定义了一个名为 'is_int' 的概念，它检查一个类型是否是 'int'
-template <class T>
-concept is_int = std::is_same_v<T, int>;
-
-void test(is_int auto a) {
-    std::println("is int");
-}
-
-// 这段代码与上面的等价
-template <is_int T>
-void test_1(T a) {
-    std::println("is int");
-}
-
-// 约束表达式支持使用 '&&' 和 '||' 进行组合，以实现更复杂的约束
-
-// ‘require’ 只需要能通过编译就可以了
-template <class T>
-concept Test = requires(T a, T b) {
-    { a + b } -> is_int; // 复合约束
-};
-
-template <class T>
-void test_2(T a)
-    requires Test<T>
-{
-    std::println("is int");
-}
+namespace stdv = std::ranges::views;
 
 auto main() -> int {
-    test(1);   // 传入一个 int 类型的参数
-    test_1(1); // 传入一个 int 类型的参数
-    test_2(1); // 传入一个 int 类型的参数
-    // test(1.0); // 传入一个 double 类型的参数，这行代码会导致编译错误，因为它不满足 is_int 的约束
+    std::array array{ 1, 2, 3, 4, 5 };
+
+    // 先过滤出偶数，再将偶数乘以2
+    for (const auto& i :
+         array | stdv::filter([](int i) { return i % 2 == 0; }) | stdv::transform([](int i) { return i * 2; })) {
+        std::cout << i << ' ';
+    }
+
+    std::cout << std::endl;
+
+    int arr[5][5] = {};
+    for (int n = 0; auto& i : arr | stdv::join) {
+        i = ++n;
+    }
+    for (const auto& i : arr | stdv::join) {
+        std::cout << i << ' ';
+    }
 }
